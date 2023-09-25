2462eadb66d01c5f8b60913eeb6bb91c018f7cc8
akash-248 <122606161+akash-248@users.noreply.github.com>
2023-09-25 11:20:20 +0530

    Update variables.js

--- basics-01/variables.js
+++ basics-01/variables.js
@@ -1,1 +1,12 @@
+const accountId = 356776;
+let accountEmail = "akash@gmail.com";
+var accountPassword = "34567";
+accountCity = "Nashik";
+
+
+// accountId = 2; // not allowed because of it is a constant variable.
+
+accountEmail = "rxtcyug@Email.com";
+accountPassword = "354678987";
+accountCity = "Sambhajinagar";
 
